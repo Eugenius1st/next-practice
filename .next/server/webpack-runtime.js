b81939c2d12dba69259e3e63c34effff9798c56e
eugenius1st <eugenius1st@gmail.com>
2025-07-13 17:59:26 +0900

    fix: generateMetadata 대문자 오타 수정

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e9807ddf68fe21b2")
+/******/ 		__webpack_require__.h = () => ("f8d5195116563237")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
