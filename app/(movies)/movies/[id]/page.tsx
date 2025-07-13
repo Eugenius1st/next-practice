ee32263948da79de8dd024a54f08152cb3b48a06
eugenius1st <eugenius1st@gmail.com>
2025-07-13 17:59:08 +0900

    fix: generateMetadata2 대문자 오타 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -6,7 +6,7 @@ interface IParamas {
   params: { id: string };
 }
 
-export async function generateMetadata({ params: { id } }: IParamas) {
+export async function generateMetadata2({ params: { id } }: IParamas) {
   const movie = await getMovie(id); // 영화 정보를 불러오기 위해 API 를 부르면 안좋은가? -> 최신버전은 fetch한번하면 캐싱된 응답을 받아서 괜찮다 ㅎㅎ
   return {
     title: movie.title,
