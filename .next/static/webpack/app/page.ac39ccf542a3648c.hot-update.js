e366ca7c1b990baa235397c8f0cb68eea3731236
eugenius1st <eugenius1st@gmail.com>
2025-07-13 18:34:56 +0900

    (home) 그룹 폴더 제거

--- .next/static/webpack/app/page.ac39ccf542a3648c.hot-update.js
+++ .next/static/webpack/app/page.ac39ccf542a3648c.hot-update.js
@@ -6,7 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/page"],{
+self["webpackHotUpdate_N_E"]("app/page",{
 
 /***/ "(app-pages-browser)/./components/movie.tsx":
 /*!******************************!*\
@@ -159,11 +159,4 @@ eval(__webpack_require__.ts("// extracted by mini-css-extract-plugin\nmodule.exp
 
 /***/ })
 
-},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["main-app"], () => (__webpack_exec__("(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Feugenius1st%2Feugene-it%2Fnext-practice%2Fcomponents%2Fmovie.tsx%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!")));
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ _N_E = __webpack_exports__;
-/******/ }
-]);
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,30 @@
-export { default } from "./(home)/page";
+import Movie from "../components/movie";
+import Link from "next/link";
+import style from "../../style/home.module.css";
+import { API_URL } from "./constants";
+export const metadata = {
+  title: "Home",
+};
+
+async function getMovies() {
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL, { cache: "force-cache" });
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return (
+    <div className={style.container}>
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          id={movie.id}
+        />
+      ))}
+    </div>
+  );
+}
